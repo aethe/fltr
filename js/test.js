70523a55e5c5876ed011266e98d3f2db9f60fb9e
Andrey Ufimtsev <andrew.aethe@gmail.com>
2016-04-17 16:11:36 +0200

    Adjust grid spacing and control size

--- js/test.js
+++ js/test.js
@@ -137,7 +137,9 @@ function runTest() {
     var image = new Image();
     // image.src = "https://i.ytimg.com/vi/Wq4Y7ztznKc/maxresdefault.jpg";
     // image.src = "http://www.prague.eu/file/edee/2014/09/prague-winter-pink-sky.jpg";
-    image.src = "http://cdn.c.photoshelter.com/img-get/I0000y3wwyf_58qM/s/750/san-francisco-baker-beach-golden-gate-bridge-california108828.jpg";
+    // image.src = "http://cdn.c.photoshelter.com/img-get/I0000y3wwyf_58qM/s/750/san-francisco-baker-beach-golden-gate-bridge-california108828.jpg";
+    image.src = "http://cs635100.vk.me/v635100859/7163/6kTTdKuy-Gw.jpg";
+    // image.src = "http://cs635100.vk.me/v635100859/7c4e/goougMymzGY.jpg";
     image.onload = function() {
         canvas.width = image.width;
         canvas.height = image.height;
